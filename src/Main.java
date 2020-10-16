bb4f46d8462a27bb7f77c354af9ee436c86ff0a8
VadimSurits <Sourits-moscow@mail.ru>
2020-10-16 22:09:39 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        // задание 1.3
+//        String abc = "Hello, world!";
+//
+//        byte a = -100;
+//        short b = 0;
+//        int c = 20000;
+//        long d = 500L;
+//        float f = 13.25f;
+//        double g = -573.007;
+//        char h = '\u2242';
+//        boolean i = false;
+//
+//        SvoyClass svoyClass = new SvoyClass("svoyClass");
+//
+//        System.out.println(abc);
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(f);
+//        System.out.println(g);
+//        System.out.println(h);
+//        System.out.println(i);
+//        svoyClass.printInConsole();
+//    }
+//}
---
+++ src/SvoyClass.java
@@ -0,0 +1,12 @@
+//public class SvoyClass {
+//
+//    String name;
+//
+//    public SvoyClass(String name) {
+//        this.name = name;
+//    }
+//
+//    void printInConsole(){
+//        System.out.println(name);
+//    }
+//}
